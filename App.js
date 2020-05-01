744ff43c9a68bcc593ecd7b1276d819b8f8d13fa
Caio Flavio <caioflavio2@hotmail.com.br>
2020-05-01 20:47:44 -0300

    :sparkles: feat(weather-app): adicionando consulta a api de temperatura

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 import Weather from './components/weather/Weather';
 import { styles } from './components/styles/app-styles';
-// import { API_KEY } from './utils/WeatherAPIKey';
+import { API_KEY } from './components/weather/utils/WeatherAPIKey';
 
 export default class App extends React.Component {
   state = {
@@ -13,11 +13,50 @@ export default class App extends React.Component {
     error: null
   };
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error getting weather conditions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 0, lng = 0) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&APPID='+API_KEY+'&units=metric')
+    .then( res => res.json())
+    .then( json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp,
+        weatherCondition: json.weather[0].main,
+        weatherDescription: json.weather[0].description,
+        locationName: json.name,
+        feelsLike: json.main.feels_like, 
+        isLoading: false
+      });
+    });
+  }
+
   render() {
     const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        {isLoading ? <Text>Fetching The Weather</Text> : <Weather />}
+        {
+          isLoading 
+          ? <Text>Fetching The Weather</Text> 
+          : <Weather 
+              weather={this.state.weatherCondition} 
+              temperature={this.state.temperature}
+              location={this.state.locationName}
+              description={this.state.weatherDescription}
+              feels_like={this.state.feelsLike}
+            />
+            }
       </View>
     );
   }
--- components/weather/Weather.js
+++ components/weather/Weather.js
@@ -4,16 +4,16 @@ import { View, Text, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { styles } from './styles/weather-styles';
 
-const Weather = ({ weather, temperature }) => {
+const Weather = ({ weather, temperature, location, description, feels_like }) => {
     return (
         <View style={styles.weatherContainer}>
             <View style={styles.headerContainer}>
                 <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
-                <Text style={styles.tempText}>{temperature}˚</Text>
+                <Text style={styles.tempText}>{location} - {temperature}˚</Text>
             </View>
             <View style={styles.bodyContainer}>
                 <Text style={styles.title}>{weather}</Text>
-                <Text style={styles.subtitle}>It hurts my eyes!</Text>
+                <Text style={styles.subtitle}>{description}</Text>
             </View>
         </View>
     );
